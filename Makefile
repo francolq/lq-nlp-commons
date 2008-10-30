246a1e0f7725c142219a9a8fc25e31ce405da503
Franco M. Luque <francolq@famaf.unc.edu.ar>
2008-10-30 05:21:19 +0000

    Agrego Makefile y setup.py. Pongo version 0.1.0.

---
+++ Makefile
@@ -0,0 +1,12 @@
+# Copyright (C) 2007-2008 Franco M. Luque
+# URL: <http://www.cs.famaf.unc.edu.ar/~francolq/>
+# For license information, see LICENSE.txt
+
+dist:
+	python setup.py sdist
+
+clean:
+	rm -f `find . -name '*.pyc'`
+	rm -f `find . -name '*~'`
+	rm -f MANIFEST
+	rm -rf dist
---
+++ setup.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2007-2008 Franco M. Luque
+# URL: <http://www.cs.famaf.unc.edu.ar/~francolq/>
+# For license information, see LICENSE.txt
+
+from distutils.core import setup
+
+setup(name='lq-nlp-commons',
+         version='0.1.0',
+         description="Franco M. Luque's Common Python Code for NLP",
+         author='Franco M. Luque',
+         author_email='francolq@famaf.unc.edu.ar',
+         url='http://www.cs.famaf.unc.edu.ar/~francolq/',
+         packages=['dep'],
+         py_modules=['bracketing', 'lbranch', 'paramdict',
+            'treebank', 'wsj10', 'cast3lb', 'model', 'rbranch',
+            'ubound', 'cast3lb10', 'negra', 'sentence', 'util',
+            'eval', 'negra10', 'setup', 'wsj'],
+         license='GNU General Public License',
+        )
