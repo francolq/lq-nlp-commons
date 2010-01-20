c6e6d6725477995dd127142243ded4cf7a79ae82
Franco M. Luque <francolq@famaf.unc.edu.ar>
2010-01-20 06:22:24 +0000

    bugfix: change usage of deptrees with depsets in lhead and rhead.

--- dep/lhead.py
+++ dep/lhead.py
@@ -1,7 +1,7 @@
 # lhead.py: LHEAD baseline for unsupervised dependency parsing.
 
-from . import model
-from . import deptree
+from dep import model
+from dep import depset
 
 class LHead(model.DepModel):
     trained = True
@@ -9,11 +9,31 @@ class LHead(model.DepModel):
     
     def __init__(self, treebank=None):
         model.DepModel.__init__(self, treebank)
-        self.Parse = [deptree.lhead_deptree(b.length) for b in self.Gold]
+        self.Parse = [depset.lhead_depset(b.length) for b in self.Gold]
 
 
 def main():
+    print "WSJ10"
     import dep.dwsj
     tb = dep.dwsj.DepWSJ10()
     m = LHead(tb)
     m.eval()
+
+"""
+from dep import lhead
+lhead.main()
+
+WSJ10
+Number of Trees: 7422
+  Directed Accuracy: 23.7
+  Undirected Accuracy: 55.6
+Debe dar: 24.0, 55.9.
+
+>>> m.count_length_2_1 = True
+>>> m.eval()
+Number of Trees: 7422
+  Directed Accuracy: 24.0
+  Undirected Accuracy: 55.7
+(52248, 0.23974123411422446, 0.55726534986985143)
+Mas cerca...
+"""
--- dep/rhead.py
+++ dep/rhead.py
@@ -1,7 +1,7 @@
 # rhead.py: RHEAD baseline for unsupervised dependency parsing.
 
-from . import model
-from . import deptree
+from dep import model
+from dep import depset
 
 class RHead(model.DepModel):
     trained = True
@@ -9,11 +9,22 @@ class RHead(model.DepModel):
     
     def __init__(self, treebank=None):
         model.DepModel.__init__(self, treebank)
-        self.Parse = [deptree.rhead_deptree(b.length) for b in self.Gold]
+        self.Parse = [depset.rhead_depset(b.length) for b in self.Gold]
 
 
 def main():
+    print "WSJ10"
     import dep.dwsj
     tb = dep.dwsj.DepWSJ10()
     m = RHead(tb)
     m.eval()
+
+"""
+from dep import rhead
+rhead.main()
+
+WSJ10
+Number of Trees: 7422
+  Directed Accuracy: 33.5
+  Undirected Accuracy: 56.4
+"""
